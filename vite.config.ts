351d7c9ffc193b3525074d1e830c18202ef35b3b
DinhTran-Netalabs <dinhtran@netalabs.com>
2025-07-09 14:02:23 +0700

    fix : fix file vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
+});
